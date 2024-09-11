69a123365f75c26de684794ff3abb6f18892f07a
A.R. Jibon <atikurrahaman386@gmail.com>
2024-09-11 16:25:30 +0600

    Add initial setup script and configuration files
    - Create `setup.sh` to streamline system setup with package
    installations and configs
    - Add scripts for installing yay, Neovim, and Docker
    - Install additional tools like Google Chrome, bash-completion, and
    lazygit
    - Set up Hyprland configuration and Bluetooth services
    - Copy bash aliases and Neovim config from dotfiles

---
+++ setup.sh
@@ -0,0 +1,23 @@
+sudo pacman -Syu
+
+# setup shortcuts
+cp dotfiles/config/hypr ~/.config -r
+
+./scripts/yay.sh
+./scripts/neovim.sh
+./scripts/docker.sh
+
+#install chrome
+yay -S google-chrome
+
+# auto bash-completion
+sudo pacman -S bash-completion
+
+sudo pacman -S lazygit
+
+cp dotfiles/.bashrc ~/ -r
+
+sudo pacman -S zip unzip bluez bluez-utils blueman
+
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
